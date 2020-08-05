a3d23f34b9c0b1b0b7350a6f6c473b586453761f
Picono435 <57836766+Picono435@users.noreply.github.com>
2020-08-05 19:15:45 +0100

    Updated to MC1.14.4-2.1
    This version is the start of the new original RTP system! This system will be safer, faster and more optimized than the old one ( /spreadplayers command ). Remember that this new system it's still on beta so if you find any issue send me a message on discord, to change from the old system to the new one just change the option use-original to true. Also this version was not possible to make without the incredible code of GeiloUtils (https://github.com/Geilokowski/GeiloUtils). Here is a detailed change-log:
    - Created a new system based on GeiloUtils of the /rtp command
    - Added the ability to choose between the New System and Old System in the config (use-original)
    - Solved minor issues
    
    How to update:
    1. Delete the old .jar file from the mods folder of the server root
    2. Move the new .jar to the folder mods of the server root

--- src/main/java/com/gmail/picono435/randomtp/commands/RTPDCommand.java
+++ src/main/java/com/gmail/picono435/randomtp/commands/RTPDCommand.java
@@ -2,6 +2,7 @@ package com.gmail.picono435.randomtp.commands;
 
 import java.math.BigDecimal;
 import java.util.HashMap;
+import java.util.Random;
 
 import com.gmail.picono435.randomtp.MainMod;
 import com.gmail.picono435.randomtp.config.Config;
@@ -9,11 +10,14 @@ import com.gmail.picono435.randomtp.config.Messages;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.command.CommandSource;
 import net.minecraft.command.Commands;
 import net.minecraft.command.arguments.DimensionArgument;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.world.World;
 import net.minecraft.world.border.WorldBorder;
@@ -24,31 +28,33 @@ public class RTPDCommand {
 	
 	private static HashMap<String, Long> cooldowns = new HashMap<String, Long>();
 	
+	private static Block[] dangerBlockArray = { Blocks.LAVA, Blocks.WATER, Blocks.AIR };
+	
 	public static void register(CommandDispatcher<CommandSource> dispatcher) {
 		dispatcher.register(Commands.literal("rtpd").requires(source -> hasPermission(source))
 				.then(
 						Commands.argument("dimension", DimensionArgument.getDimension())
 						.executes(context -> 
-						randomTeleport(context.getSource().asPlayer(), DimensionArgument.func_212592_a(context, "dimension"))
+						runCommand(context.getSource().asPlayer(), DimensionArgument.func_212592_a(context, "dimension"))
 						)
 				));
 		dispatcher.register(Commands.literal("randomteleportdimension").requires(source -> hasPermission(source))
 				.then(
 						Commands.argument("dimension", DimensionArgument.getDimension())
 						.executes(context -> 
-						randomTeleport(context.getSource().asPlayer(), DimensionArgument.func_212592_a(context, "dimension"))
+						runCommand(context.getSource().asPlayer(), DimensionArgument.func_212592_a(context, "dimension"))
 						)
 				));
 		dispatcher.register(Commands.literal("randomtpd").requires(source -> hasPermission(source))
 				.then(
 						Commands.argument("dimension", DimensionArgument.getDimension())
 						.executes(context ->
-						randomTeleport(context.getSource().asPlayer(), DimensionArgument.func_212592_a(context, "dimension"))
+						runCommand(context.getSource().asPlayer(), DimensionArgument.func_212592_a(context, "dimension"))
 						)
 				));
 	}
 	
-	private static int randomTeleport(PlayerEntity p, DimensionType dim) {
+	private static int runCommand(PlayerEntity p, DimensionType dim) {
 		World world = p.getEntityWorld();
 	    WorldBorder border = world.getWorldBorder();
 	    MinecraftServer server = MainMod.server;
@@ -69,6 +75,16 @@ public class RTPDCommand {
 	    		return 1;
 	    	}
 	    	p.changeDimension(dim);
+	    	if(Config.useOriginal.get()) {
+	    		randomTeleport(p);
+	    		cooldowns.put(p.getName().getString(), System.currentTimeMillis());
+	    		return 1;
+	    	}
+	    	if(Config.useOriginal.get()) {
+	    		randomTeleport(p);
+	    		cooldowns.put(p.getName().getString(), System.currentTimeMillis());
+	    		return 1;
+	    	}
 	        if(Config.max_distance.get() == 0) {
 	        	server.getCommandManager().handleCommand(server.getCommandSource(), "spreadplayers " + border.getCenterX() + " " + border.getCenterZ() + " " + Config.min_distance.get() + " " + maxDistance + " false " + p.getName().getString().toLowerCase());
 	        	p.sendMessage(succefull);
@@ -119,4 +135,69 @@ public class RTPDCommand {
 			return false;
 		}
 	}
+	
+	private static void randomTeleport(PlayerEntity p) {
+		try {
+			Random r = new Random();
+			  int low = Config.min_distance.get();
+			  int high = Config.max_distance.get();
+			  if(high == 0) {
+				  high = (int) (p.world.getWorldBorder().getDiameter() / 2);
+			  }
+			  int x = r.nextInt(high-low) + low;
+			  int y = 50;
+			  int z = r.nextInt(high-low) + low;
+			  int maxTries = -1;
+			  while (!isSafe(p, x, y, z) && (maxTries == -1 || maxTries > 0)) {
+				  y++;
+				  if(y >= 120) {
+					  x = r.nextInt(high-low) + low;
+					  y = 50;
+					  z = r.nextInt(high-low) + low;
+					  continue;
+				  }
+				  if(maxTries > 0){
+					  maxTries--;
+				  }
+				  if(maxTries == 0) {
+					  StringTextComponent msg = new StringTextComponent("Error, please try again.");
+					  p.sendMessage(msg);
+					  return;
+				  }
+			  }
+			  
+			  p.setPositionAndUpdate(x, y, z);
+			  StringTextComponent succefull = new StringTextComponent(Messages.succefully.get().replaceAll("\\{playerName\\}", p.getName().getString()).replaceAll("\\{blockZ\\}", "" + p.getPositionVector().z).replaceAll("\\{blockX\\}", "" + p.getPositionVector().x).replaceAll("&", "§"));
+			  p.sendMessage(succefull);
+		} catch(Exception ex) {
+			MainMod.logger.info("Error executing command.");
+			ex.printStackTrace();
+		}
+	  }
+	  
+	  private static boolean isSafe(PlayerEntity player, int newX, int newY, int newZ) {
+		if ((isEmpty(player.world, newX, newY, newZ)) && 
+				(!isDangerBlock(player.world, newX, newY - 1, newZ))) {
+			return true;
+		}
+		return false;
+	  }
+		  
+	  private static boolean isEmpty(World world, int newX, int newY, int newZ) {
+		if ((world.isAirBlock(new BlockPos(newX, newY, newZ))) && (world.isAirBlock(new BlockPos(newX, newY + 1, newZ))) && 
+				(world.isAirBlock(new BlockPos(newX + 1, newY, newZ))) && (world.isAirBlock(new BlockPos(newX - 1, newY, newZ))) && 
+				(world.isAirBlock(new BlockPos(newX, newY, newZ + 1))) && (world.isAirBlock(new BlockPos(newX, newY, newZ - 1)))) {
+			return true;
+		}
+		return false;
+	  }
+		  
+	  private static boolean isDangerBlock(World world, int newX, int newY, int newZ) {
+		for (Block block : dangerBlockArray) {
+			if (block.equals(world.getBlockState(new BlockPos(newX, newY, newZ)).getBlock())) {
+				return true;
+			}
+		}
+		return false;
+	  }
 }
