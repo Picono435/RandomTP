57856bb41de7c09aa00dcfeec42be6930400e204
Picono435 <57836766+Picono435@users.noreply.github.com>
2021-08-10 21:44:40 +0100

    Fixed major issue with cooldown exempt permission

--- fabric/src/main/java/com/gmail/picono435/randomtp/api/fabric/RandomTPAPIImpl.java
+++ fabric/src/main/java/com/gmail/picono435/randomtp/api/fabric/RandomTPAPIImpl.java
@@ -3,6 +3,7 @@ package com.gmail.picono435.randomtp.api.fabric;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import me.lucko.fabric.api.permissions.v0.Permissions;
 import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.server.level.ServerPlayer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,6 +14,10 @@ public class RandomTPAPIImpl {
 
 	public static boolean hasPermission(CommandSourceStack source, String permission) {
 		return Permissions.check(source, permission, registeredNodes.get(permission));
+	}
+
+	public static boolean hasPermission(ServerPlayer player, String permission) {
+		return Permissions.check(player, permission, registeredNodes.get(permission));
 	}
 
 }
--- forge/src/main/java/com/gmail/picono435/randomtp/api/forge/RandomTPAPIImpl.java
+++ forge/src/main/java/com/gmail/picono435/randomtp/api/forge/RandomTPAPIImpl.java
@@ -2,16 +2,21 @@ package com.gmail.picono435.randomtp.api.forge;
 
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraftforge.server.permission.PermissionAPI;
 
 public class RandomTPAPIImpl {
-	
+
 	public static boolean hasPermission(CommandSourceStack source, String permission) {
 		try {
 			return PermissionAPI.hasPermission(source.getPlayerOrException(), permission);
 		} catch (CommandSyntaxException e) {
 			return false;
 		}
+	}
+
+	public static boolean hasPermission(ServerPlayer player, String permission) {
+		return PermissionAPI.hasPermission(player, permission);
 	}
 
 }
