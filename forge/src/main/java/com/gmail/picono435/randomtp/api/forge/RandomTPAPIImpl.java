e64d4bc67671034c83bfde1afed64237c4b1ab62
Picono435 <picono435@gmail.com>
2022-01-22 12:52:29 +0000

    Added support for the new PermissionsAPI from forge, fixed crash on launch (#8)

--- forge/src/main/java/com/gmail/picono435/randomtp/api/forge/RandomTPAPIImpl.java
+++ forge/src/main/java/com/gmail/picono435/randomtp/api/forge/RandomTPAPIImpl.java
@@ -1,5 +1,6 @@
 package com.gmail.picono435.randomtp.api.forge;
 
+import com.gmail.picono435.randomtp.forge.RandomTPModForge;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.server.level.ServerPlayer;
@@ -9,14 +10,30 @@ public class RandomTPAPIImpl {
 	
 	public static boolean hasPermission(CommandSourceStack source, String permission) {
 		try {
-			return PermissionAPI.hasPermission(source.getPlayerOrException(), permission);
+			if(permission.equalsIgnoreCase("randomtp.command.basic")) {
+				return PermissionAPI.getPermission(source.getPlayerOrException(), RandomTPModForge.BASIC_COMMAND_PERM);
+			} else if(permission.equalsIgnoreCase("randomtp.command.interdim")) {
+				return PermissionAPI.getPermission(source.getPlayerOrException(), RandomTPModForge.INTERDIM_COMMAND_PERM);
+			} else if(permission.equalsIgnoreCase("randomtp.cooldown.exempt")) {
+				return PermissionAPI.getPermission(source.getPlayerOrException(), RandomTPModForge.COOLDOWN_EXEMPT_PERM);
+			} else {
+				return true;
+			}
 		} catch (CommandSyntaxException e) {
 			return false;
 		}
 	}
 
 	public static boolean hasPermission(ServerPlayer player, String permission) {
-		return PermissionAPI.hasPermission(player, permission);
+		if(permission.equalsIgnoreCase("randomtp.command.basic")) {
+			return PermissionAPI.getPermission(player, RandomTPModForge.BASIC_COMMAND_PERM);
+		} else if(permission.equalsIgnoreCase("randomtp.command.interdim")) {
+			return PermissionAPI.getPermission(player, RandomTPModForge.INTERDIM_COMMAND_PERM);
+		} else if(permission.equalsIgnoreCase("randomtp.cooldown.exempt")) {
+			return PermissionAPI.getPermission(player, RandomTPModForge.COOLDOWN_EXEMPT_PERM);
+		} else {
+			return true;
+		}
 	}
 
 }
