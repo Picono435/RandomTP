abb1e9826536460c4b332b23ade0b15eb1648346
Picono435 <picono435@gmail.com>
2022-12-22 13:45:54 +0000

    Increased max Y to make sure it can reach most of biomes

--- common/src/main/java/com/gmail/picono435/randomtp/api/RandomTPAPI.java
+++ common/src/main/java/com/gmail/picono435/randomtp/api/RandomTPAPI.java
@@ -64,7 +64,7 @@ public class RandomTPAPI {
             while (!isSafe(world, mutableBlockPos) && (maxTries == -1 || maxTries > 0)) {
                 y++;
                 mutableBlockPos.setY(y);
-                if(mutableBlockPos.getY() >= 150 || !isInBiomeWhitelist(world.getBiome(mutableBlockPos.immutable()).unwrapKey().get().location())) {
+                if(mutableBlockPos.getY() >= 200 || !isInBiomeWhitelist(world.getBiome(mutableBlockPos.immutable()).unwrapKey().get().location())) {
                     if(biomeResourceKey != null) {
                         Pair<BlockPos, Holder<Biome>> pair = world.findClosestBiome3d(biomeHolder -> biomeHolder.is(biomeResourceKey), mutableBlockPos.immutable(), 6400, 32, 64);
                         if(pair == null) {
@@ -75,6 +75,7 @@ public class RandomTPAPI {
                         mutableBlockPos.setX(pair.getFirst().getX());
                         mutableBlockPos.setY(50);
                         mutableBlockPos.setZ(pair.getFirst().getZ());
+                        continue;
                     }
                     int x = random.ints(boundsX.getFirst(), boundsX.getSecond()).findAny().getAsInt();
                     if(random.nextInt(2) == 1) x = x * -1;
